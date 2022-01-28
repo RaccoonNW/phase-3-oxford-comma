90b3e772d3879c6ae12f2a48ad047f34001b8b54
Gabriel <shalitmontagne.g@gmail.com>
2022-01-27 19:17:42 -0700

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length < 3
+        array.join(" and ")
+    elsif array.length >= 3
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
